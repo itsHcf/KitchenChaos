7ae18cfbfadc39dabfff9d3130c078d6b5baacb7
hcf <502984075@qq.com>
2025-12-13 21:42:39 +0800

    add containercounter animation

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ContainerCounter : BaseCounter
 {
+
+    public event EventHandler OnPlayerGrabbedObject;
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     public override void Interact(Player player)
     {
@@ -14,5 +17,7 @@ public class ContainerCounter : BaseCounter
         Transform tomatoTransform = Instantiate(kitchenObjectSO.prefab);
         KitchenObject kitchenObject = tomatoTransform.GetComponent<KitchenObject>();
         kitchenObject.SetBaseCounter(player);
+
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
     }
 }
