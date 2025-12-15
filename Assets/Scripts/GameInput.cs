1426925ed24328b5f91bd5aa553870198f1f33e8
hcf <502984075@qq.com>
2025-12-15 13:52:15 +0800

    添加 Dispose

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,17 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
+
+    public static GameInput Instance { get; private set; }
     public event EventHandler OnInteractAction;
+    public event EventHandler OnPauseAction;
     private InputActions inputActions;
     private void Awake()
     {
+
+        Instance = this;
         inputActions = new InputActions();
         inputActions.Player.Enable();
         inputActions.Player.Interact.performed += Interact_performed;
+        inputActions.Player.Pause.performed += Pause_performed;
+    }
+
+    private void OnDestroy()
+    {
+        inputActions.Player.Interact.performed -= Interact_performed;
+        inputActions.Player.Pause.performed -= Pause_performed;
+        inputActions.Player.Disable();
+        inputActions.Dispose();
+    }
+
+    private void Pause_performed(InputAction.CallbackContext context)
+    {
+        OnPauseAction?.Invoke(this, EventArgs.Empty);
     }
 
     private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
--- Assets/Scripts/MyGameManager.cs
+++ Assets/Scripts/MyGameManager.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MyGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public static MyGameManager Instance { get; private set; }
 
+    public event EventHandler OnGamePaused;
+    public event EventHandler OnGameUnpaused;
+
+    public bool IsPaused { get; private set; } = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        GameInput.Instance.OnPauseAction += GameInput_OnPauseAction;
+    }
 
+    private void GameInput_OnPauseAction(object sender, EventArgs e)
+    {
+        Pause();
+    }
+
+    public void Pause()
+    {
+        IsPaused = !IsPaused;
+        if (IsPaused)
+        {
+            Time.timeScale = 0f;
+            OnGamePaused?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            OnGameUnpaused?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
